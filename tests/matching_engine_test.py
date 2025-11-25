4f8075d532c4220ac6866ab8faa6acd215bf76a7
azakharove <azakharov@uchicago.edu>
2025-11-24 22:14:23 -0500

    slight test refactoring

--- tests/matching_engine_test.py
+++ tests/matching_engine_test.py
@@ -1,7 +1,7 @@
 from trading_lib.matching_engine import MatchingEngine
 from trading_lib.models import Order, OrderStatus
 
-def test_submitting_order_with_id():
+def test_filling_order_with_id():
     engine = MatchingEngine(cancel_rate=0.0, partial_fill_rate=0.0)
     order = Order(symbol="AAPL", quantity=10, price=150, status=OrderStatus.PENDING, id="custom_id_123")
     
@@ -15,10 +15,10 @@ def test_submitting_order_with_id():
     processed_order = engine.process_order(order)
     assert_order_atrb(processed_order)
 
-def test_submitting_order_without_id():
+def test_filling_order_without_id():
     engine = MatchingEngine(cancel_rate=0.0, partial_fill_rate=0.0)
     order = Order(symbol="AAPL", quantity=10, price=150, status=OrderStatus.PENDING)
-    
+
     def assert_order_atrb(ord: Order):
         assert ord.id is not None
         assert ord.status == OrderStatus.FILLED
@@ -26,6 +26,8 @@ def test_submitting_order_without_id():
     
     engine.subscribe_order_updates(assert_order_atrb)
     
+    assert order.id is None
+
     processed_order = engine.process_order(order)
     assert_order_atrb(processed_order)
 
@@ -51,20 +53,6 @@ def test_cancelled_order():
         assert ord.id is not None
         assert ord.status == OrderStatus.CANCELED
         assert ord.filled_quantity == 0
-    
-    engine.subscribe_order_updates(assert_order_atrb)
-    
-    processed_order = engine.process_order(order)
-    assert_order_atrb(processed_order)
-
-def test_fully_filled_order():
-    engine = MatchingEngine(cancel_rate=0.0, partial_fill_rate=0.0)  # Force full fill
-    order = Order(symbol="AAPL", quantity=15, price=150, status=OrderStatus.PENDING)
-    
-    def assert_order_atrb(ord: Order):
-        assert ord.id is not None
-        assert ord.status == OrderStatus.FILLED
-        assert ord.filled_quantity == 15
     
     engine.subscribe_order_updates(assert_order_atrb)
     
